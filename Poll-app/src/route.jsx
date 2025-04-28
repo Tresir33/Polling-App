995ee9bd98e733573a84f6ac20e9dfcdb33bff90
Tresir33 <tresilaho33@gmail.com>
2025-04-28 11:30:10 +0200

    updating my Arboraisence

---
+++ Poll-app/src/route.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+const route = () => {
+  return (
+    <>
+    <Route>
+        <Routes>
+            
+        </Routes>
+    </Route>
+      
+    </>
+  )
+}
+
+export default route
